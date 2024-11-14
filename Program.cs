6f4b8ecef54ad852eb55198efecf72d5ce953bce
guilherme-barros <guilhermebarros27102006@gmail.com>
2024-11-13 22:42:15 -0300

    commit3

--- Program.cs
+++ Program.cs
@@ -1,8 +1,17 @@
+using Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<DataContext>
+(
+    options => 
+    {
+        options.UseSqlServer(builder
+            .Configuration.GetConnectionString("ConexaoLocal"));
+    }
+);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
